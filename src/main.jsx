42b4015581369e99f562ffda05d11009b80b82b2
ankerderbanker <anker2000@hotmail.com>
2025-09-03 10:17:15 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router";
-import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+// main.jsx or App.jsximport React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 import "./main.css";
 
-function App() {
-  return (
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <App />
     </BrowserRouter>
-  );
-}
-export default App;
+  </React.StrictMode>
+);
