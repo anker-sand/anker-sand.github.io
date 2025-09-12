538ceeeb514c37303dfbe80473f8c4c866572fbc
ankerderbanker <anker2000@hotmail.com>
2025-09-12 20:20:54 +0200

    Commitcube

---
+++ src/components/Room/objects/InteractiveObject.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import "./InteractiveObject.css";
+
+export default function InteractiveObject({
+  image,
+  className,
+  tooltipText,
+  onClick,
+}) {
+  const [tooltip, setTooltip] = useState({ visible: false, x: 0, y: 0 });
+
+  const handleMouseEnter = (e) => {
+    setTooltip({ visible: true, x: e.clientX, y: e.clientY });
+  };
+
+  const handleMouseMove = (e) => {
+    setTooltip((prev) => ({ ...prev, x: e.clientX, y: e.clientY }));
+  };
+
+  const handleMouseLeave = () => {
+    setTooltip({ visible: false, x: 0, y: 0 });
+  };
+
+  return (
+    <>
+      <img
+        src={image}
+        className={`interactive-object ${className}`}
+        onMouseEnter={handleMouseEnter}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        onClick={onClick}
+      />
+
+      {tooltip.visible && (
+        <div
+          className="tooltip"
+          style={{
+            position: "fixed", // ensure fixed, not relative
+            top: tooltip.y + 20,
+            left: tooltip.x + 20,
+          }}
+        >
+          {tooltipText}
+        </div>
+      )}
+    </>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,30 @@
-import roomBase from "../assets/images/room_img/Room_base.png";
+import roomBase from "../assets/images/room/theroom.png";
+import desk from "../assets/images/room/desk.png";
+import meinchair from "../assets/images/room/meinchair.png";
+import projectwall from "../assets/images/room/projectwall.png";
+import door from "../assets/images/room/door.png";
+import mail from "../assets/images/room/mail.png";
+import InteractiveObject from "../components/Room/objects/InteractiveObject";
 import "./Home.css";
-//import Cat from "../components/Room/objects/Cat";
 
-export default function Home() {
+export default function Home({ onNavigate }) {
   return (
     <div className="home-container">
-      <img src={roomBase} alt="Room base" className="room-base" />
+      <div className="room-wrapper">
+        <img src={roomBase} className="room-base" />
+        <img src={desk} className="desk" />
+
+        <InteractiveObject
+          image={meinchair}
+          className="meinchair"
+          tooltipText="About Me"
+          onClick={() => onNavigate("about")}
+        />
+
+        <img src={projectwall} className="projectwall" />
+        <img src={door} className="door" />
+        <img src={mail} className="mail" />
+      </div>
     </div>
   );
 }
